1c39758e59e8314f22ab2195e19f77e62a15054e
ivant8k <13523129@std.stei.itb.ac.id>
2026-05-10 23:37:53 +0700

    feat: init

---
+++ custom_addons/bootcamp_fapet/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Bootcamp Fapet Dashboard",
+    'summary': 'Sistem Dashboard Terintegrasi Bootcamp Fapet Unpad',
+    'description': 'Sistem Dashboard Terintegrasi Bootcamp Fapet Unpad',
+    'sequence': -100,
+    'author': "Kelompok 08 - K03",
+    'category': 'Uncategorized',
+    'version': '1.0',
+    'depends': ['base', 'web'],
+    'data': [
+        'security/bootcamp_groups.xml',
+        'security/ir.model.access.csv',
+        'views/bootcamp_menus.xml',
+    ],
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
+    # 'qweb': [
+    # ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
