f00ad55fe2a636496c4b99dd23889d217a73285a
ivant8k <13523129@std.stei.itb.ac.id>
2026-05-13 23:54:26 +0700

    fix: sidebar biaya operasional n pos sync ga keliatan

--- custom_addons/bootcamp_fapet/controllers/biaya_operasional.py
+++ custom_addons/bootcamp_fapet/controllers/biaya_operasional.py
@@ -17,16 +17,32 @@ JENIS_BIAYA_LABEL = {
 
 class BiayaOperasionalController(http.Controller):
 
+    def _get_permissions(self, user):
+        groups = user.sudo().groups_id
+        is_it_staff = user.has_group('bootcamp_fapet.group_staff_it')
+        return {
+            'utama': True if is_it_staff else any(getattr(g, 'x_bootcamp_utama', False) for g in groups),
+            'keuangan': any(getattr(g, 'x_bootcamp_keuangan', False) for g in groups),
+            'pengadaan': any(getattr(g, 'x_bootcamp_pengadaan', False) for g in groups),
+            'kpi': any(getattr(g, 'x_bootcamp_kpi', False) for g in groups),
+            'input_biaya': any(getattr(g, 'x_bootcamp_input_biaya', False) for g in groups),
+            'input_stok': any(getattr(g, 'x_bootcamp_input_stok', False) for g in groups),
+            'input_kpi': any(getattr(g, 'x_bootcamp_input_kpi', False) for g in groups),
+            'target_kpi': any(getattr(g, 'x_bootcamp_target_kpi', False) for g in groups),
+            'sinkron_pos': any(getattr(g, 'x_bootcamp_sinkron_pos', False) for g in groups),
+            'hak_akses': True if is_it_staff else any(getattr(g, 'x_bootcamp_hak_akses', False) for g in groups),
+            'log_sistem': True if is_it_staff else any(getattr(g, 'x_bootcamp_log_sistem', False) for g in groups),
+            'is_direktur': user.has_group('bootcamp_fapet.group_direktur'),
+            'is_kepala_keuangan': user.has_group('bootcamp_fapet.group_kepala_keuangan'),
+            'is_manajer_operasional': user.has_group('bootcamp_fapet.group_manajer_operasional'),
+            'is_kepala_prosesing': user.has_group('bootcamp_fapet.group_kepala_prosesing'),
+            'is_staff_it': is_it_staff,
+        }
+
     @http.route('/bootcamp/biaya-operasional', type='http', auth='user', website=False)
     def input_biaya_operasional(self, **kw):
         user = request.env.user
-        user_groups = {
-            'is_direktur': user.has_group('bootcamp_fapet.group_direktur'),
-            'is_kepala_keuangan': user.has_group('bootcamp_fapet.group_kepala_keuangan'),
-            'is_kepala_prosesing': user.has_group('bootcamp_fapet.group_kepala_prosesing'),
-            'is_manajer_operasional': user.has_group('bootcamp_fapet.group_manajer_operasional'),
-            'is_staff_it': user.has_group('bootcamp_fapet.group_staff_it'),
-        }
+        user_groups = self._get_permissions(user)
 
         biaya_list = request.env['bootcamp.biaya.operasional'].sudo().search(
             [], order='tanggal desc, id desc', limit=10
